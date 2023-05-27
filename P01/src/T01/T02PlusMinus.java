b2e64e9602dd7e59c4e732e03a0759cfb8411d15
YoungHyoKim90 <younghyo1101@gmail.com>
2023-05-27 14:47:38 +0900

    print culc

---
+++ P01/src/T01/T02PlusMinus.java
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+package T01;
+
+public class T02PlusMinus {
+	public static void main(String[] args) {
+
+		System.out.println(1+2);
+		System.out.println(2+3);
+		System.out.println(3+4);
+		
+		System.out.println("1+2");
+		System.out.println("2+3");
+		System.out.println("3+4");
+		
+		System.out.println(3-2);
+		System.out.println(1*2);
+		System.out.println(4/2);
+		
+		System.out.println(1+1.3);
+		System.out.println(1-3);
+		System.out.println(1*1.5);
+		//System.out.println(1/0); -- 컴파일에러 //는 주석처리! 즉 컴퓨터는 안읽습니다!
+		
+		System.out.println(5/3);
+		
+		System.out.println((double)5/3);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
