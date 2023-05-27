74eebdb6c2b8733781d2df7d168f120b09259d82
YoungHyoKim90 <younghyo1101@gmail.com>
2023-05-27 14:09:56 +0900

    자바자바 안녕하세요

---
+++ P01/src/T01/T01Print_Println.java
@@ -0,0 +1,13 @@
+package T01;
+
+public class T01Print_Println {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hi");
+		System.out.print("안녕하세요");
+
+	}
+
+}
