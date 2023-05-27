7a296cc4982e7d93370076deeaf988183b531009
YoungHyoKim90 <younghyo1101@gmail.com>
2023-05-27 15:22:10 +0900

    T03

---
+++ P01/src/T01/T03Variable.java
@@ -0,0 +1,26 @@
+package T01;
+
+public class T03Variable {
+	public static void main(String[] args) {
+
+		//변수 선언하기!
+		// 변수타입 변수명 이건 숫자정수를 저장할 수 있는 X라는 이름의 기억저장 장소다! 기억하라 멍청한 컴퓨터... 아니 사랑스런 컴퓨터야!
+		// int라는 타입의 x라는 이름의 변수를 선언하겠습니다. 자바는 마지막에 ;를 사용하여 명령어를 실행시킵니다.
+		
+		int x ;
+	
+		x = 1 ;
+		
+		int y ;
+		
+	    y = 2;
+	    
+	    //System.out.println(x+y);
+	    
+	    int result ;
+	    
+	    result = x+y;
+	    
+	    System.out.println(result);
+	}
+}
