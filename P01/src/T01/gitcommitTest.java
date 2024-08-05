50c1ab463659aa936d3b1caa04adb81a9bf6c248
NNPC999 <NNPC999@DESKTOP-PQ5595B>
2024-08-05 14:12:37 +0900

    test2

--- P01/src/T01/gitcommitTest.java
+++ P01/src/T01/gitcommitTest.java
@@ -3,7 +3,7 @@ package T01;
 public class gitcommitTest {
 	public static void main(String[] args) {
 		System.out.println("git commit AMS P01");
-		
-		
+		System.out.println("git commit AMS P01");
+		System.out.println("git commit AMS P01");
 	}
 }
